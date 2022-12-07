c04a0844d073cda0989ebc2f338c17f58125007b
Marcosdg1221 <114072151+Marcosdg1221@users.noreply.github.com>
2022-12-07 08:22:53 +0100

    Update Main.java

--- src/ejerciciospracticos/Main.java
+++ src/ejerciciospracticos/Main.java
@@ -1,10 +1,12 @@
 package ejerciciospracticos;
 
-import java.io.PrintStream;
+//pongo java util
 import java.util.Scanner;
 
+//Inicio el main
+
 public class Main {
-		
+		//Hago un void y genero las variables esPrimo y Capicua, son booleanas, y un int num1.
 	public static void main(String[] args) {
 		boolean esPrimo;
 		boolean esCapicua;
@@ -13,9 +15,12 @@ public class Main {
 		
 			Scanner all = new Scanner(System.in); //pongo scanner
 			System.out.println("Introduzca un número");
+		//El número a pedir es num1, cogerá su valor.
 
 		num1 = all.nextInt();
+		//Genero una nueva clase llamada números.
 		Numeros resultado = new Numeros();
+		//Cuando ya los tenga, pongo resultado.
 		System.out.println("El número es primo" + resultado.esPrimo(num1));
 		System.out.println("El número es capicua" + resultado.esCapicua(num1));
 	}
